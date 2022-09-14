8be97929f8c05dbaca6318bea55eb17dff611ecd
Joorkar Romero <joorkarobrajham@gmail.com>
2022-09-13 19:07:02 -0600

    starting with cycles

--- js/index.js
+++ js/index.js
@@ -1,9 +1,68 @@
+const inputBulbasaur = document.getElementById('bulbasaur')
+const inputCharmander = document.getElementById('charmander')
+const inputSquirtle = document.getElementById('squirtle')
+
+const spanPetPlayer = document.getElementById('pet-player')
+const spanPetEnemy = document.getElementById('pet-enemy')
+const spanPlayerlifes = document.getElementById('player-lifes')
+const spanEnemylifes = document.getElementById('enemy-lifes')
+
+const resultMessage = document.getElementById('result')
+
+const playerAttacksMessage = document.getElementById('player-attacks')
+const enemyAttacksMessage = document.getElementById('enemy-attacks')
+
+const buttonFire = document.getElementById('button-fire')
+const buttonWater = document.getElementById('button-water')
+const buttonPlant = document.getElementById('button-plant')
+
+const buttonPetPlayer = document.getElementById('button-pet');
+const restartButton = document.getElementById('button-restart');
+
+let pokemones = []
 let attackPlayer
 let attackEnemy
-let playerLives = 3
-let enemyLives = 3
+let playerlifes = 3
+let enemylifes = 3
 
-let sectionInicialNone = [
+class Pokemon {
+  constructor(name, photo, life) {
+    this.name = name
+    this.photo = photo
+    this.life = life
+    this.attacks = []
+  }
+}
+
+let bulbasaur = new Pokemon('Bulbasaur', './assets/Bulbasaur.png', 5)
+let charmander = new Pokemon('Charmander', './assets/Charmander.png', 5)
+let squirtle = new Pokemon('Squirtle', './assets/Squirtle.png', 5)
+
+bulbasaur.attacks.push(
+  { name: '💧', id: 'button-water' },
+  { name: '💧', id: 'button-water' },
+  { name: '💧', id: 'button-water' },
+  { name: '🔥', id: 'button-fire' },
+  { name: '🌱', id: 'button-plant' },
+)
+
+charmander.attacks.push(
+  { name: '🔥', id: 'button-fire' },
+  { name: '🔥', id: 'button-fire' },
+  { name: '🔥', id: 'button-fire' },
+  { name: '💧', id: 'button-water' },
+  { name: '🌱', id: 'button-plant' },
+)
+
+squirtle.attacks.push(
+  { name: '🌱', id: 'button-plant' },
+  { name: '🌱', id: 'button-plant' },
+  { name: '🌱', id: 'button-plant' },
+  { name: '🔥', id: 'button-fire' },
+  { name: '💧', id: 'button-water' },
+)
+
+const sectionInicialNone = [
   document.getElementById('select-attack'),
   document.getElementById('restart'),
   document.getElementById('select-pet'),
@@ -11,12 +70,9 @@ let sectionInicialNone = [
 sectionInicialNone[0].style.display = 'none'
 sectionInicialNone[1].style.display = 'none'
 
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
 const createMessage = (result) => {
-  let resultMessage = document.getElementById('result')
-  let playerAttacksMessage = document.getElementById('player-attacks')
-  let enemyAttacksMessage = document.getElementById('enemy-attacks')
-
   let newPlayerAttack = document.createElement('p')
   let newEnemyAttack = document.createElement('p')
 
@@ -29,8 +85,6 @@ const createMessage = (result) => {
 }
 
 const createFinalMessage = (finalResult) => {
-  let resultMessage = document.getElementById('result')
-
   resultMessage.innerHTML = finalResult
 
   buttonFire.disabled = true
@@ -40,18 +94,15 @@ const createFinalMessage = (finalResult) => {
   sectionInicialNone[1].style.display = 'block'
 }
 
-const checkLives = () => {
-  if (enemyLives === 0) {
+const checklifes = () => {
+  if (enemylifes === 0) {
     createFinalMessage(`Congratulations! You Win 😄`)
-  } else if (playerLives === 0) {
+  } else if (playerlifes === 0) {
     createFinalMessage(`Sorry, you lose 😔`)
   }
 }
 
 const combat = () => {
-  let spanPlayerLives = document.getElementById('player-lives')
-  let spanEnemyLives = document.getElementById('enemy-lives')
-
   switch (true) {
     case attackEnemy === attackPlayer:
       createMessage('DEAD HEAT 😑')
@@ -60,17 +111,17 @@ const combat = () => {
       || attackEnemy === 'WATER' && attackPlayer === 'FIRE'
       || attackEnemy === 'PLANT' && attackPlayer === 'WATER':
       createMessage('WIN ✅')
-      enemyLives--
-      spanEnemyLives.innerHTML = enemyLives
+      enemylifes--
+      spanEnemylifes.innerHTML = enemylifes
       break;
     default:
       createMessage('LOSE ❌')
-      playerLives--
-      spanPlayerLives.innerHTML = playerLives
+      playerlifes--
+      spanPlayerlifes.innerHTML = playerlifes
       break;
   }
 
-  checkLives()
+  checklifes()
 }
 
 const attackRandomEnemy = () => {
@@ -106,18 +157,12 @@ const attackPlant = () => {
   attackRandomEnemy()
 }
 
-let buttonFire = document.getElementById('button-fire')
-  buttonFire.addEventListener('click', attackFire)
-let buttonWater = document.getElementById('button-water')
-  buttonWater.addEventListener('click', attackWater)
-let buttonPlant = document.getElementById('button-plant')
-  buttonPlant.addEventListener('click', attackPlant)
-
-const random = (min,max) => Math.floor(Math.random() * (max - min + 1) + min)
+buttonFire.addEventListener('click', attackFire)
+buttonWater.addEventListener('click', attackWater)
+buttonPlant.addEventListener('click', attackPlant)
 
 const selectPetEnemy = () => {
-  let petRandom = random(1,3)
-  let spanPetEnemy = document.getElementById('pet-enemy')
+  let petRandom = random(1, 3)
 
   switch (true) {
     case petRandom === 1:
@@ -135,11 +180,6 @@ const selectPetEnemy = () => {
 const selectPetPlayer = () => {
   sectionInicialNone[2].style.display = 'none'
   sectionInicialNone[0].style.display = 'flex'
-
-  let inputBulbasaur = document.getElementById('bulbasaur')
-  let inputCharmander = document.getElementById('charmander')
-  let inputSquirtle = document.getElementById('squirtle')
-  let spanPetPlayer = document.getElementById('pet-player')
 
   switch (true) {
     case inputBulbasaur.checked:
@@ -160,12 +200,10 @@ const selectPetPlayer = () => {
   selectPetEnemy()
 }
 
-let buttonPetPlayer = document.getElementById('button-pet');
 buttonPetPlayer.addEventListener('click', selectPetPlayer);
 
 const restartGame = () => {
   location.reload();
 }
 
-let restartButton = document.getElementById('button-restart');
 restartButton.addEventListener('click', restartGame)
